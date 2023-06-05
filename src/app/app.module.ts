c69358ab352ce80859dcf1b34df15fb5686a4208
LL208 <amantialucio@hotmail.it>
2023-06-05 12:28:58 +0200

    Prova di push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { SergioComponent } from './sergio/sergio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    SergioComponent
   ],
   imports: [
     BrowserModule,
