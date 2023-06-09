c3f71a59ab4ff50554bd471ff7ae0ac41419dc02
NinoHub994 <antoninopirri94@gmail.com>
2023-06-09 12:58:45 +0200

    added card

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Card } from 'src/app/models/Card';
 
 @Component({
   selector: 'app-card',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
-
+  @Input() card!:Card;
 }
