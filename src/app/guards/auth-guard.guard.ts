6b01cd9607d98554dc88ca6941c6bf2e559af266
LL208 <amantialucio@hotmail.it>
2023-06-10 12:33:59 +0200

    LocalStorage completed

---
+++ src/app/guards/auth-guard.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable, inject } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { UserService } from '../services/user.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  userService = inject(UserService);
+  router = inject(Router);
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {  
+    
+      if (JSON.parse(this.userService.getData('isLogged'))) {
+        return true;
+    } else {
+      alert('Non puoi entrare');
+      this.router.navigate(['login']);
+      return false;
+    }
+    
+  }
+  
+}
