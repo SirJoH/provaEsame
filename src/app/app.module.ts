d707a44937603cc7280b668778316c29b97f7e21
NinoHub994 <antoninopirri94@gmail.com>
2023-06-10 12:37:02 +0200

    modifing listing component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
@@ -28,6 +28,7 @@ import { ListingComponent } from './components/listing/listing.component';
     ListingComponent
   ],
   imports: [
+    HttpClientModule,
     FormsModule,
     BrowserModule,
     AppRoutingModule,
--- src/app/components/listing/listing.component.ts
+++ src/app/components/listing/listing.component.ts
@@ -1,26 +1,40 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, inject } from '@angular/core';
 import { Subject } from 'rxjs';
+import { Card } from 'src/app/models/Card';
 import { Data } from 'src/app/models/Data';
 
 @Component({
   selector: 'app-listing',
   templateUrl: './listing.component.html',
-  styleUrls: ['./listing.component.scss']
+  styleUrls: ['./listing.component.scss'],
 })
-export class ListingComponent implements OnInit{
-
-  data = new Subject<any>();
-
-
+export class ListingComponent {
+  response = new Subject<Card[]>();
+  cards: Card[] = [];
   http = inject(HttpClient);
 
   ngOnInit(): void {
     this.getData();
+    this.response.subscribe((cardData) => {
+      this.cards=cardData;
+    });
+    console.log("cards",this.cards);
   }
   getData = () => {
-    this.http.get<Data[]>('https://randomuser.me/api/?results=1000&seed=contactgram&nat=us,fr,gb&exc=login,registered&noinfo')
-    .subscribe(item => item.map((user)=>  String(user.name.first+" "+ user.name.last)));
-  }  
-  
+    this.http
+      .get<Data[]>(
+        'https://randomuser.me/api/?results=1000&seed=contactgram&nat=us,fr,gb&exc=login,registered&noinfo'
+      )
+      .subscribe((items) => {
+        if (Array.isArray(items) && items.length > 0) {
+          const cardData: Card[] = items.map((user) => ({
+            title: user.name.first,
+            description: user.name.last,
+            srcImg: user.picture.medium,
+          }));
+          this.response.next(cardData);
+        }
+      });
+  };
 }
